a3aa33e27b6c808bc719edc65bc58683b886aa6d
sameerali306 <sameeraly240@gmail.com>
2025-06-30 12:08:27 +0500

    quote generatore

---
+++ quote-generater/src/App.jsx
@@ -0,0 +1,14 @@
+import './index.css'
+import Quote from './quoteGenerater/Quote'
+
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <Quote/>
+    </div>
+  )
+}
+
+export default App
